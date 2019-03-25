10d7b589c552844acac031cbfcbd5c8db62ddf7d
Michael <mnewell3@zagmail.gonzaga.edu>
2019-03-25 14:17:14 -0700

    Made some basic shapes representing landscape

--- main.java
+++ main.java
@@ -10,10 +10,31 @@ public class main extends JPanel
 	{
 		super.paintComponent(g);
 		g.setColor(Color.BLACK);
-		int []xarr = {0, 300, 600};
-		int []yarr = {900, 0, 900};
+		setBackground(Color.CYAN);
+
+		//first mountain
+		int []xarr = {200, 400, 600};
+		int []yarr = {800, 100, 800};
+		g.fillPolygon(xarr, yarr, 3);
+	
+		//second mountain
+ 		xarr = new int[] {0, 200, 400};
+		yarr = new int[] {800, 100, 800};
+		g.setColor(Color.YELLOW);
 		g.fillPolygon(xarr, yarr, 3);
 		addListeners();
+
+		//third mountain
+ 		xarr = new int[] {400, 600, 800};
+		yarr = new int[] {800, 100, 800};
+		g.setColor(Color.GRAY);
+		g.fillPolygon(xarr, yarr, 3);
+		addListeners();
+
+		//ground
+		Color brown = new Color(150,75,0);
+		g.setColor(brown);
+		g.fillRect(0, 675, 800, 50);
 	}
 	
 	public static void main(String[] args)
@@ -22,8 +43,10 @@ public class main extends JPanel
 		JFrame frame = new JFrame();
 		
 		frame.setTitle("Motion Parallax");
-		frame.setSize(900,900);
+		frame.setSize(800,800);
+		
 		frame.setVisible(true);
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(m);
 		
