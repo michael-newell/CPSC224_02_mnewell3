e8bd6e7f3e11de420f4ccc6296f36205705804c9
Michael <mnewell3@zagmail.gonzaga.edu>
2019-03-26 19:02:25 -0700

    update

--- main.java
+++ main.java
@@ -1,133 +1,190 @@
-import java.awt.Graphics;
+package motionparallax;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.Timer;
 import javax.swing.*;
+import java.awt.Graphics;
 import java.awt.*;
 import java.awt.event.*;
 
-
-public class main extends JPanel
-{
-	static int windowW = 800;
-	static int windowH = 800;
-	int centerX = windowW / 2;
-	int centerY = windowH / 2;	
-	
-	int []mountain1x = {(windowW / 4), (windowW / 4) * 2, (windowW / 4) * 3};
-	int []mountain1y = {windowH, 100, windowH};
-
-	int []mountain2x = {0, (windowW / 4), (windowW / 4) * 2};
-	int []mountain2y = {windowH, 100, windowH};
-
-	int []mountain3x = {(windowW / 4) * 2, (windowW / 4) * 3, windowW};
-	int []mountain3y = {windowH, 100, windowH};
-	
-	
-
-	public void paintComponent(Graphics g)
-	{
-		super.paintComponent(g);
-		g.setColor(Color.BLACK);
-		setBackground(Color.CYAN);
-
-		//first mountain
-		g.fillPolygon(mountain1x, mountain1y, 3);
-	
-		//second mountain
-		g.setColor(Color.YELLOW);
-		g.fillPolygon(mountain2x, mountain2y, 3);
-		addListeners();
-
-		//third mountain
-		g.setColor(Color.GRAY);
-		g.fillPolygon(mountain3x, mountain3y, 3);
-		addListeners();
-
-		//ground
-		Color brown = new Color(150,75,0);
-		g.setColor(brown);
-		g.fillRect(0, 675, 800, 300);
-	}
-	
-	public static void main(String[] args)
-	{
-		main m = new main();
-		JFrame frame = new JFrame();
-		
-		frame.setTitle("Motion Parallax");
-		frame.setSize(windowW, windowH);
-		
-		frame.setVisible(true);
-		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(m);
-		
-	}
-	public void addListeners()
-	{
-		addMouseListener(new MyMListener());
-		addMouseMotionListener(new MyMMListener());
-	}
-   public class MyMListener implements MouseListener
-   {
-      public void mousePressed(MouseEvent e)
-      {
-         
-      }
-
-      public void mouseClicked(MouseEvent e)
-      {
-                
-      }
-
-      public void mouseReleased(MouseEvent e)
-      {
-         
-      }
-
-      public void mouseEntered(MouseEvent e)
-      {
+public class MotionParallax extends JFrame implements ActionListener {
+    //basic var stuff
+    private int MouseX;
+    private int MouseY;
+    private final int W = 510;
+    private final int H = 548; //top bar is 38 pixels...
+    
+    //Mouse information for listeners - shows up as text in panel
+    private int click = 0;
+    private String clicks = "Clicks: ";
+    private String position = "Pos: --, --";
+    private boolean drag = false;
+    private String dragPosition = "Drag: --, --";
+    private String mouseInOut = "---";
+    private String mousePressed = "Mouse press: No";
+    
+    private Timer timeListener; //timeListener
+    
+    private Color skyBlue = new Color(135, 206, 250); //background color
+    
+    //PARALLAX...............
+    private int sunParallax = 35;
+    private int mountainParallax = 125;
+    private int treeTrunkParallax = 80;
+    private int treeLeavesParallax = 80;
+    private int grassParallax = 100;
+    
+    public MotionParallax(){
+        setTitle("Assignment 4: Motion Parallax"); //title
+        getContentPane().setBackground(skyBlue); //backgroud color
+        setSize(W, H); //Width/Height of box, couldn't stop it from being resized...
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //close when exit
         
-      }
-
-      public void mouseExited(MouseEvent e)
-      {
-         mountain1x = new int[]{(windowW / 4), (windowW / 4) * 2, (windowW / 4) * 3};
-	 mountain1y = new int[]{windowH, 100, windowH};
-
-	 mountain2x = new int[]{0, (windowW / 4), (windowW / 4) * 2};
-	 mountain2y = new int[]{windowH, 100, windowH};
-
-	 mountain3x = new int[]{(windowW / 4) * 2, (windowW / 4) * 3, windowW};
-	 mountain3y = new int[]{windowH, 100, windowH};
-      }
-   }
-
-   public class MyMMListener implements MouseMotionListener
-   {  
-      public void mouseDragged(MouseEvent e)
-      {
+        //mouse listeners
+        addMouseListener(new MyMouseListener());
+        addMouseMotionListener(new MyMouseMotionListener());
         
-      }
+        //timer for drawing
+        timeListener = new Timer(100, this);
+        timeListener.start();
+        
+        //show panel
+        setVisible(true);
+    }
+    
+    public void paint(Graphics g){
+        super.paint(g); //paint method
+        
+        //mouse properties - from listeners
+        g.drawString(position, 70, 80);
+        g.drawString(clicks, 70, 100);
+        g.drawString(mouseInOut, 70, 120);
+        g.drawString(dragPosition, 70, 140);
+        g.drawString(mousePressed, 70, 160);
+        
+        //new colors
+        //Color name = new Color(R,G,B)
+        Color skyBlue = new Color(135, 206, 250);
+        Color brown = new Color(80, 30, 30);
+        Color darkGreen = new Color(0, 158, 34);
+        Color grass = new Color(1, 198, 28);
+        Color gray = new Color(142, 142, 142);
+        
+        //polygon coordinates
+        int mountainX[] = {0, 400, 200};
+        int mountainY[] = {470, 470, 100};
+        
+        int triangleTreeX[] = {345, 425, 385};
+        int triangleTreeY[] = {385, 385, 300};
+        
+        int points = 3; //for some reason fillPolygon needs points as a var...
+        
+        //draw singular bird
+        //g.setColor(Color.black);
+        //g.drawLine
+        
+                /* References for shapes:
+                g.fillRect(x, y, Width, Height);
+                g.drawLine(x1, y1, x2, y2);
+                --  top bar checker:  --
+                g.setColor(Color.BLACK);
+                g.fillRect(x, y, WIDTH, HEIGHT);
+                bar is about 38 pixels
+                g.fillArc(50, 50, 50, 50, 50, 50);
+                g.fillArc(x, y, w, h, startArc, arcAngle);
+                g.
+                */
 
-      public void mouseMoved(MouseEvent e)
-      {
-	int mouseX = e.getX();
-	int mouseY = e.getY();
-	if (mouseX > centerX + 10)
-	{
-		for (int i = 0; i < mountain1x.length; i++)
-		{
-			mountain1x[i] += 3;
-			mountain2x[i] += 2;
-			mountain3x[i] += 1;
-		}
-		if (mouseX > 500)
-		{
-			centerX = 500;
-		}
-	}
-
-	
-	repaint();		 
-      }
-   }
+        
+        //mountain
+        for(int i=0; i<3; i++){
+            mountainX[i] += (MouseX/mountainParallax);
+            mountainY[i] += (MouseY/mountainParallax);
+        }
+        g.setColor(gray);
+        g.fillPolygon(mountainX, mountainY, points);
+        
+        //grass
+        g.setColor(grass);
+        g.fillRect(0/*+(MouseX/grassParallax)*/, 438+(MouseY/grassParallax), 500, 100);
+                //the X coord is kinda pointless becuase the grass stays the same left and right...
+        
+        //tree (trunk and leaves)
+        g.setColor(brown);
+        g.fillRect(370+(MouseX/treeTrunkParallax), 380+(MouseY/treeTrunkParallax), 30, 80);
+        for(int i=0; i<3; i++){
+            triangleTreeX[i] += (MouseX/treeLeavesParallax);
+            triangleTreeX[i] += (MouseY/treeLeavesParallax);
+        }
+        g.setColor(darkGreen);
+        g.fillPolygon(triangleTreeX, triangleTreeY, points);
+        
+        //sun
+        g.setColor(Color.yellow);
+        g.fillOval(390+(MouseX/sunParallax), 80+(MouseY/sunParallax), 50, 50);
+        
+        
+        
+        
+        /*
+        //red reference bars - only used for testing and getting oriented
+        g.setColor(Color.red);
+        //x
+        g.drawLine(0, 138, 500, 138);
+        g.drawLine(0, 238, 500, 238);
+        g.drawLine(0, 338, 500, 338);
+        g.drawLine(0, 438, 500, 438);
+        g.drawLine(0, 538, 500, 538);
+        //y
+        g.drawLine(100, 0, 100, 538);
+        g.drawLine(200, 0, 200, 538);
+        g.drawLine(300, 0, 300, 538);
+        g.drawLine(400, 0, 400, 538);
+        g.drawLine(500, 0, 500, 538);
+        */
+    }
+    
+    //when the timeListener activates the panel will repaint
+    public void actionPerformed(ActionEvent e){
+        repaint();
+    }
+    
+    
+    private class MyMouseMotionListener implements MouseMotionListener{
+        public void mouseMoved(MouseEvent e){
+            MouseX = e.getX();
+            MouseY = e.getY();
+            position = "Move: " + MouseX + ", " + MouseY;
+            //repaint();
+        }
+        public void mouseDragged(MouseEvent e) {
+            MouseX = e.getX();
+            MouseY = e.getY();
+            dragPosition = "Drag: " + MouseX + ", " + MouseY;
+        }
+    }
+    
+    private class MyMouseListener implements MouseListener{
+        public void mouseClicked(MouseEvent e) {
+            click++;
+            clicks = "Clicks: " + click;
+        }
+        public void mousePressed(MouseEvent e) {
+            mousePressed = "Mouse press: Yes";
+        }
+        public void mouseReleased(MouseEvent e) {
+            mousePressed = "Mouse press: No";
+        }
+        public void mouseEntered(MouseEvent e) {
+            mouseInOut = "Entered";
+        }
+        public void mouseExited(MouseEvent e) {
+            mouseInOut = "Exited";
+        }
+    }
+    
+    public static void main(String[] args) {
+        new MotionParallax();
+    }
+    
 }
